512ab353438d5a76c32ec77b73cfa61274409ab9
ericmahan <ericmahan0408@gmail.com>
2025-03-15 04:07:51 -0400

    Reformatted some parts. Replaced float 'f' with a randomly-generated floating-point number. Printed floats have more digits.

--- fastfloat.c
+++ fastfloat.c
@@ -1,8 +1,8 @@
 #include <stdint.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
-
 
 float double_float(float f) {
     uint32_t i;
@@ -28,15 +28,14 @@ float half_float(float f) {
     exponent--;
 
     i = (i & ~0x7f800000) | (exponent << 23);
-
     memcpy(&result, &i, sizeof(result));
 
     return result;
 }
 
-
 int main() {
-    float f = 97.381f;
+    srand(time(0)); // Seed the RNG
+    float random = (float) rand() / 1073741824;
     int numIterations = 10000000;
 
     float fastDoubleResult = 0.0f;
@@ -44,38 +43,39 @@ int main() {
     float dubb = 0.0f;
     float halve = 0.0f;
 
+    printf("Original float, \'f\': %.24f\n", random);
 
     clock_t start = clock(); // Time double_float function
     for (int i = 0; i < numIterations; i++) {
-        fastDoubleResult = double_float(f);
+        fastDoubleResult = double_float(random);
     }
     clock_t end = clock();
     double doubleFloatTime = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("double_float function time: %f seconds (result: %f)\n", doubleFloatTime, fastDoubleResult);
+    printf("double_float function time: %.8f seconds (result: %.24f)\n", doubleFloatTime, fastDoubleResult);
 
     start = clock(); // Time half_float function
     for (int i = 0; i < numIterations; i++) {
-        fastHalfResult = half_float(f);
+        fastHalfResult = half_float(random);
     }
     end = clock();
     double halfFloatTime = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("half_float function time: %f seconds (result: %f)\n", halfFloatTime, fastHalfResult);
+    printf("half_float function time: %.8f seconds (result: %.24f)\n", halfFloatTime, fastHalfResult);
 
     start = clock(); // Time regular doubling w/ multiplication
     for (int i = 0; i < numIterations; i++) {
-        dubb = f * 2.0f;
+        dubb = random * 2.0f;
     }
     end = clock();
     double multiplyTime = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Regular doubling time: %f seconds (result: %f)\n", multiplyTime, dubb);
+    printf("Regular doubling time: %.8f seconds (result: %.24f)\n", multiplyTime, dubb);
     
     start = clock(); // Time regular halving w/ division
     for (int i = 0; i < numIterations; i++) {
-        halve = f / 2.0f;
+        halve = random / 2.0f;
     }
     end = clock();
     double divideTime = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Regular halving time: %f seconds (result: %f)\n", divideTime, halve);
+    printf("Regular halving time: %.8f seconds (result: %.24f)\n", divideTime, halve);
 
     return 0;
 }
